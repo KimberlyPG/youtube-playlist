ce427db584c92d8dc28e4df9e6f311cab5c229ad
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-06-30 17:09:27 -0600

    add collection to firestore
    new add colletion and documents function in firebase file

--- src/features/videos/videoSlice.ts
+++ src/features/videos/videoSlice.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { useEffect } from 'react';
 import { RootState, AppThunk } from '../../app/store';
+import { addCollectionAndDocuments } from '../../utils/firebase/firebase'
 
 type PlaylistProps = {
     id: string;
@@ -23,12 +25,24 @@ const initialState: Array<PlaylistProps> = [
     // },
 ]
 
+// useEffect(() => {
+//     addCollectionAndDocuments('playlist', )
+    
+// }, []);
+
+const objeto = {
+    description: "Funny CATS can make you CRY OUT OF LAUGHTE",
+    url: "https://www.youtube.com/watch?v=21oQ4X2NxIs"
+}
+
 export const videoSlice = createSlice({
     name: 'video',
     initialState,
     reducers: {
         addVideo: (state, action) => {
             state.push(action.payload);
+            addCollectionAndDocuments('playlist', objeto); 
+            console.log("PAYLOAD", action.payload);
         },
         deleteVideo: (state, action) => {
             return state.filter((videoItem) => videoItem.id !== action.payload); 
--- src/utils/firebase/firebase.jsx
+++ src/utils/firebase/firebase.jsx
@@ -6,6 +6,16 @@ import {
     GoogleAuthProvider, 
     onAuthStateChanged
 } from 'firebase/auth';
+import {
+  getFirestore,
+  doc, //allows us to retrieve documents inside our firestore database
+  getDoc, //gettting document data
+  setDoc, //setting document data
+  collection,
+  writeBatch,
+  query,
+  getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyD4IEtYHDc08LL9oFXMGLsYoVT_wBrJ8ec",
@@ -32,4 +42,36 @@ export const signInWithGooglePopup = () => {
 export const onAuthStateChangedListener = (callback) => {
   onAuthStateChanged(auth, callback) 
 }
+
+//  Instance to get or set data to our document 
+export const db = getFirestore();
+
+// Add collection and documents to firestore
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db); 
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  })
+
+  await batch.commit();
+  console.log('done');
+}
+
+// // get collection from firestore
+// export const getCategoriesAndDocuments = async () => {
+//   const collectionRef = collection(db, 'categories');
+//   const q = query(collectionRef);
+
+//   const querySnapshot = await getDocs(q);
+//   const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+//     const { title, items } = docSnapshot.data();
+//     acc[title.toLowerCase()] = items;
+//     return acc;
+//   }, {})
+
+//   return categoryMap;
+// }
 
