e0a18acf7baf2f0f2ef4f3145dafe59fa355a0dc
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-07-13 17:31:56 -0700

    playlist-video
    new playlist-video that contains each video in the playlist

---
+++ src/containers/Playlist-video/playlist-video.styles.tsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+export const PlaylistVideoContainer = styled.div`
+    margin-top: 10%;
+    img {
+        width: 95%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export const Title = styled.div`
+    font-size: 17px;
+    height: 57px;
+    font-weight: bold;
+    font-family: "Roboto","Arial";
+    position: relative;
+`;
+
+export const PlaylistLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+`;
+
+export const Icon = styled.div`
+    size: 50px;
+    opacity: 0.7;
+    position: absolute;
+    bottom: 20px;
+    right: 50px;
+    display: none;
+    border: none;
+`
+
+export const VideoPlayer = styled.div`
+    height: 200px;
+    position: relative;
+    
+    &:hover {
+        img {
+            opacity: 0.8;
+        }
+
+        ${Icon} {
+            opacity: 0.85;
+            display: flex;
+        }
+    }
+`;
+
+export const Button = styled.button`
+    position: absolute; left:0px; bottom:0px;
+    opacity: 0.8;
+    font-size: 10px;
+`
